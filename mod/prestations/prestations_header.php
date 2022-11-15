91321ee2c63eb6abfc54a87fcdfe9e673188df7d
LucasTamaya <lucastamaya.974@gmail.com>
2022-11-15 16:29:20 +0400

    maj ui prestations côté client

--- mod/prestations/prestations_header.php
+++ mod/prestations/prestations_header.php
@@ -1,10 +1,14 @@
 <?php
 
-$html = ' <h1>prestations</h1>';
-$html .= '<h2><a href="index.php?page=prestations&fk_categorie=1">  Epilations  </a></h2>';
-$html .= '<h2><a href="index.php?page=prestations&fk_categorie=2">  Soins  </a></h2>';
-$html .= '<h2><a href="index.php?page=prestations&fk_categorie=3">  Cils/Sourcils  </a></h2>';
-$html .= '<h2><a href="index.php?page=prestations&fk_categorie=4">  Onglerie  </a></h2>';
+$html = ' <h1 class="text-center text-3xl text-blue-900 font-semibold my-5">Prestations</h1>';
+$html .= '<nav>';
+$html .= '      <ul class="max-w-3xl flex justify-evenly mx-auto bg-blue-900 p-5 rounded mb-10">';
+$html .= '          <li><a class="text-lg text-yellow-300 font-bold" href="index.php?page=prestations&fk_categorie=1">Epilations</a></li>';
+$html .= '          <li><a class="text-lg text-yellow-300 font-bold" href="index.php?page=prestations&fk_categorie=2">Soins</a></li>';
+$html .= '          <li><a class="text-lg text-yellow-300 font-bold" href="index.php?page=prestations&fk_categorie=3">Cils / Sourcils</a></li>';
+$html .= '          <li><a class="text-lg text-yellow-300 font-bold" href="index.php?page=prestations&fk_categorie=4">Onglerie</a></li>';
+$html .= '      </ul>';
+$html .= '</nav>';
 
 
 if (isset($_GET['fk_categorie'])) {
@@ -19,11 +23,11 @@ if (isset($_GET['fk_categorie'])) {
         while ($data_produits = mysqli_fetch_assoc($rs_categorie)) {
 
             $html .= '<div>';
-            $html .= '<h3> ' . $data_produits['titre'] . ' </h3>';
             $html .= '<img src="images/produits/' . $data_produits['fichier'] . '" alt="' . $data_produits['description'] . '" </img>';
-            $html .= '<p> ' . $data_produits['prix'] . ' </p>';
-            $html .= '<p> ' . $data_produits['temps'] . ' </p>';
-            $html .= '<p> ' . $data_produits['description'] . ' </p>';
+            $html .= '<h2>' . $data_produits['titre'] . '</h2>';
+            $html .= '<p>' . $data_produits['prix'] . '</p>';
+            $html .= '<p>' . $data_produits['temps'] . '</p>';
+            $html .= '<p>' . $data_produits['description'] . '</p>';
             $html .= '<button id="' . $data_produits['id'] . '"> Ajouter au panier </button>';
             $html .= '</div>';
         }
@@ -31,18 +35,21 @@ if (isset($_GET['fk_categorie'])) {
 } else {
     $sql_categorie = "SELECT * FROM t_produit";
     $rs_categorie = query($sql_categorie);
+    $html .= '<div class="max-w-7xl mx-auto grid grid-cols-3 gap-5">';
     //on stoque dans data
     if ($rs_categorie && mysqli_num_rows($rs_categorie)) {
         while ($data_produits = mysqli_fetch_assoc($rs_categorie)) {
 
             $html .= '<div>';
-            $html .= '<h3> ' . $data_produits['titre'] . ' </h3>';
-            $html .= '<img src="images/produits/' . $data_produits['fichier'] . '" alt="' . $data_produits['description'] . '" </img>';
-            $html .= '<p> ' . $data_produits['prix'] . ' </p>';
-            $html .= '<p> ' . $data_produits['temps'] . ' </p>';
-            $html .= '<p> ' . $data_produits['description'] . ' </p>';
-            $html .= '<button id="' . $data_produits['id'] . '"> Ajouter au panier </button>';
+            $html .= '  <img src="images/produits/' . $data_produits['fichier'] . '" alt="' . $data_produits['description'] . '" </img>';
+            $html .= '  <h2 class="text-2xl font-bold mb-2">' . $data_produits['titre'] . '</h2>';
+            $html .= '  <div class="flex justify-between items-center mb-2">';
+            $html .= '      <p>' . $data_produits['prix'] . '&euro;</p>';
+            $html .= '      <p>' . $data_produits['temps'] . 'min</p>';
+            $html .= '  </div>';
+            $html .= '  <button class="w-full py-2 rounded text-white text-yellow-300 font-bold bg-blue-900" id="' . $data_produits['id'] . '">Ajouter au panier</button>';
             $html .= '</div>';
         }
     }
+    $html .= '</div>';
 }
