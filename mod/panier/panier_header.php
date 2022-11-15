42840a4b1ec361544961a7724b1dbc3bd45bfa68
LucasTamaya <lucastamaya.974@gmail.com>
2022-11-15 19:42:55 +0400

    maj titre des pages + style galerie

--- mod/panier/panier_header.php
+++ mod/panier/panier_header.php
@@ -1,6 +1,7 @@
 <?php
-$html = "";
+$html = '<div class="mt-10"></div>';
 
 foreach ($_SESSION[SESSION_NAME]['panier'] as $data_panier) {
     echo $data_panier['fk_produit'] . ' / ' . $data_panier['quantite'] . '<br/>';
+    $html .= $data_panier['fk_produit'] . ' / ' . $data_panier['quantite'];
 }
