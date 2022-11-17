b583944ef0562bf4a42557b5f23bef0694ae6598
LucasTamaya <lucastamaya.974@gmail.com>
2022-11-17 13:34:13 +0400

    ajout loader pour la page paiement

--- mod/confirmation_paiement/confirmation_paiement_header.php
+++ mod/confirmation_paiement/confirmation_paiement_header.php
@@ -1,2 +1,5 @@
 <?php
-$html = '<h1 class="text-center text-blue-900 text-3xl my-5">Vous allez être redirigé vers la page de paiement!</h1>';
+$html = '<div class="w-full h-[80vh] flex flex-col justify-center items-center">';
+$html .= '  <h1 class="text-center text-blue-900 text-3xl font-bold mb-16">Vous allez être redirigé vers la page de paiement!</h1>';
+$html .= '  <div class="w-20 h-20 rounded-full border-l-4 border-t-4 animate-spin border-blue-900"></div>';
+$html .= '</div>';
